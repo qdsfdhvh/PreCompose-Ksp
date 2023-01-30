c0f3742eead4054bd6012b0b0d9acc1f24fcc44b
seiko <seiko_des@outlook.com>
2023-01-30 15:00:49 +0800

    AGP to 7.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    id("com.android.application") version "7.2.2" apply false
-    id("com.android.library") version "7.2.2" apply false
+    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
     kotlin("multiplatform") version "1.7.20" apply false
     id("com.google.devtools.ksp") version "1.7.20-1.0.7" apply false
     id("org.jetbrains.compose") version "1.2.0" apply false
